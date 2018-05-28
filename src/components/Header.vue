70a1dc81c8a68d5c6bbded49dd84b093d9825a7c
Gordon B <gordon.indeed@gmail.com>
2018-05-27 17:37:53 -0700

    Add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <img src="../assets/ablogo.png" alt="ab logo">
+    <a v-on:click="logout">
+      Logout
+      <i class="fas fa-sign-out-alt"></i>
+    </a>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+  },
+  methods: {
+    logout: () => {
+      alert("You logged out!!");
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.header {
+  height: 98px;
+  width: auto;
+  background-color: #F4EB20;
+}
+</style>
